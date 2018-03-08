3af2cfa81b0a30d4d5e14d6d53b4b17f5072f975
AJ <ajhollowayvrm@gmail.com>
2018-03-08 10:21:55 -0700

    Add Semantic UI

---
+++ web/admin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>S-Cube</title>
+
+        <link rel="stylesheet" type="text/css" href="/resources/Semantic-UI-CSS-master/semantic.css">
+    </head>
+<body>
+
+    <h1>This is a Heading</h1>
+    <p>This is a paragraph.</p>
+
+</body>
+    <script src="/resources/Semantic-UI-CSS-master/semantic.min.js"></script>
+</html>
